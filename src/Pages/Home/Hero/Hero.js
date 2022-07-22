71c99323cb4a707d4869204f79444bfefa6a2564
Ai Shadhin <aishadhin.ac@gmail.com>
2022-07-22 20:34:13 +0600

    'effect'

--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import hero from '../../../images/hero.png';
-import Zoom from 'react-reveal/Zoom';
 
 const Hero = () => {
     return (
@@ -8,9 +7,10 @@ const Hero = () => {
             <div className='container mx-auto'>
                 <div class="hero">
                     <div class="flex items-center w-full justify-between flex-col lg:flex-row-reverse">
-                        <Zoom>
-                            <img src={hero} class=" rounded-lg " alt='' />
-                        </Zoom>
+                        <img data-aos="fade-up"
+                            data-aos-delay="50"
+                            data-aos-duration="500"
+                            data-aos-once="false" src={hero} class=" rounded-lg " alt='' />
                         <div className='w-[50%]'>
                             <h1 class="text-7xl font-bold text-white uppercase">Seo guru</h1>
                             <p class="py-6 text-white">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
